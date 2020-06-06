6093e14eb87b5ea940be1a972359619afcee58ec
Lucas Souza <lucas.souza@diymaxeda.com>
2020-06-06 15:00:51 +0200

    Added apple mechanics and score counter updated

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
         let currentIndex = 0
         let direction = 1
 
-        let appleIndex = 0
+        let appleIndex = 5
         let interval = 10
+        let intervalTime = 0
         let speed = 0.9
 
         let score = 0
@@ -17,20 +18,34 @@ document.addEventListener('DOMContentLoaded', function() {
         let width = 10
 
         const startGame = () => {
+            // clear grid
             currentSnake.forEach(index => squares[index].classList.remove('snake'))
             squares[appleIndex].classList.remove('apple')
 
-
+            // reset controllers
             score = 0
             scoreDisplay.innerText = score
             interval = 100
             currentSnake = [2, 1, 0]
             currentIndex = 0
 
-            currentSnake.forEach(index => squares[index].classList.add('snake')) // clear grid
+            // add elements
+            placeApple()
+            currentSnake.forEach(index => squares[index].classList.add('snake'))
+            squares[appleIndex].classList.add('apple')
 
             setInterval(movimentController, interval);
+        }
 
+        const placeApple = () => {
+            do {
+                appleIndex = Math.floor(Math.random() * squares.length)
+                console.log(appleIndex)
+            } while (squares[appleIndex].classList.contains('snake'))
+
+            squares[appleIndex].classList.add('apple')
+
+            console.log(appleIndex)
         }
 
         const movimentController = () => {
@@ -38,20 +53,32 @@ document.addEventListener('DOMContentLoaded', function() {
                 (currentSnake[0] + width >= (width * width) && direction == width) ||
                 (currentSnake[0] % width === (width - 1) && direction == 1) ||
                 (currentSnake[0] % width === 0 && direction == -1) ||
-                (currentSnake[0] - width <= 0 && direction == -width)
+                (currentSnake[0] - width <= 0 && direction == -width) ||
+                squares[currentSnake[0] + direction].classList.contains('snake') // contains itself
             ) {
                 return clearInterval(interval)
             }
-
-            console.log('move')
 
             const tail = currentSnake.pop()
             squares[tail].classList.remove('snake')
             currentSnake.unshift(currentSnake[0] + direction)
 
+            if (squares[currentSnake[0]].classList.contains('apple')) {
+                squares[currentSnake[0]].classList.remove('apple')
+
+                placeApple()
+                score++
+                scoreDisplay.innerText = score
+
+                squares[tail].classList.add('snake')
+                currentSnake.push(tail)
+
+                intervalTime = interval * speed
+                interval = setInterval(movimentController, intervalTime)
+
+            }
+
             squares[currentSnake[0]].classList.add('snake')
-
-
 
         }
 
